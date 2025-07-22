71842e67c521661048271317014f4fbe8622046d
Facundo Tobias Rey <103077968+reyfacundo@users.noreply.github.com>
2025-07-22 05:26:55 -0300

    added env variables

--- React-Rey/src/main.jsx
+++ React-Rey/src/main.jsx
@@ -4,12 +4,12 @@ import { initializeApp } from "firebase/app";
 import App from './App.jsx'
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDPwO5fJibMfMIfQhEXcJG8lQPEuMa42TQ",
-  authDomain: "coder-react-7e688.firebaseapp.com",
-  projectId: "coder-react-7e688",
-  storageBucket: "coder-react-7e688.firebasestorage.app",
-  messagingSenderId: "623486984892",
-  appId: "1:623486984892:web:ec152c71478ccadf8e66b9"
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_ID
 };
 
 initializeApp(firebaseConfig);
