b29039ce8bcd8109332d42af435f8d71b8ff7212
Facundo Tobias Rey <103077968+reyfacundo@users.noreply.github.com>
2025-07-25 05:59:41 -0300

    added orders

--- React-Rey/src/components/NavBar/Cart/CartContent.jsx
+++ React-Rey/src/components/NavBar/Cart/CartContent.jsx
@@ -1,10 +1,12 @@
 import cartContext from "../../context";
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { Link } from "react-router-dom";
+import { addDoc, collection, getFirestore } from "firebase/firestore";
 import "./Cart.css";
 
 const CartContent = () => {
     const { products, deleteProduct } = useContext(cartContext);
+    const [orderId, setOrderId] = useState();
 
     if (products.length == 0) {
         return (
@@ -18,6 +20,24 @@ const CartContent = () => {
         );
     }
 
+    const totalPrice = +products
+        .reduce((total, prod) => total + prod.product.price * prod.quantity, 0)
+        .toFixed(2);
+
+    const createOrder = (productsArray, total) => {
+        const order = {
+            products: [...productsArray],
+            total: +total,
+        };
+        const db = getFirestore();
+        const itemCollection = collection(db, "order");
+        addDoc(itemCollection, order)
+            .then(({ id }) => {
+                setOrderId(id);
+                document.querySelector(".order").classList.remove("hidden");
+            })
+            .catch((err) => console.error("Order failed:", err));
+    };
     return (
         <div className="cartCardContainer">
             <div className="cardContainer">
@@ -29,7 +49,12 @@ const CartContent = () => {
                         >
                             <span>
                                 <h2>{prod.product.title}</h2>
-                                <h2>${prod.product.price}</h2>
+                                <h2>
+                                    $
+                                    {(
+                                        prod.product.price * prod.quantity
+                                    ).toFixed(2)}
+                                </h2>
                                 <h3>QUANTITY: {prod.quantity}</h3>
                             </span>
                         </Link>
@@ -45,8 +70,34 @@ const CartContent = () => {
                     </div>
                 ))}
             </div>
-            <div className="purchase">
+            <div className="total">
+                {/* <p>TOTAL:</p>
+                <span>
+                {totalPrice}
+                </span> */}
+                <h3>TOTAL: {totalPrice}</h3>
+            </div>
+            <div
+                className="purchase"
+                onClick={() => {
+                    createOrder(
+                        products.map((prod) => {
+                            return {
+                                quantity: prod.quantity,
+                                product: prod.product.title,
+                            };
+                        }),
+                        totalPrice
+                    );
+                }}
+            >
                 <h2>BUY</h2>
+            </div>
+            <div className="order hidden">
+                <h2>ORDER PLACED!</h2>
+                <h3>
+                    ORDER ID: <span>{orderId}</span>
+                </h3>
             </div>
         </div>
     );
